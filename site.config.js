d1da6dfd061598dae9be0ec791a329d690d39777
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 09:52:59 +0900

    start

---
+++ site.config.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  siteName: 'lg상품권 희망티켓',
+  siteUrl: 'https://gift-hh-3.pages.dev',
+  siteDescription: 'lg상품권 최고가 매입 보장. 엘지 유플러스 상품권 및 모바일 결제를 365일 24시간 안전하게 진행하세요.',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/sample.png',
+  naverVerificationCode: '',
+  googleVerificationCode: '',
+  company: '희망티켓',
+  phone: '01094691051',
+  sms: '01094691051',
+  kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
+};
